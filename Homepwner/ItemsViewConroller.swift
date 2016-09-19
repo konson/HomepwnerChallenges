30279f536c0b166217e1c958af2d7398906a2e0a
Alecs Konson <alecs429@gmail.com>
2016-09-19 13:47:01 -0400

    Initial commit

---
+++ Homepwner/ItemsViewConroller.swift
@@ -0,0 +1,15 @@
+//
+//  ItemsViewConroller.swift
+//  Homepwner
+//
+//  Created by Alecsandra Konson on 9/19/16.
+//  Copyright © 2016 Apperator. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+    
+    
+}
